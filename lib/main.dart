f8fd131f7265a68b34165ca5024de18883f0001c
Damigrace1 <ogdamtim@gmail.com>
2024-07-27 10:51:31 +0100

    Template created

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const TapCardApp());
+}
+class TapCardApp extends StatelessWidget {
+  const TapCardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(393, 852),
+        builder: (_, child) {
+          return GetMaterialApp(
+            builder: (context, child) {
+              return Overlay(
+                initialEntries: [OverlayEntry(builder: (context) => child!)],
+              );
+            },
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              useMaterial3: true,
+              scaffoldBackgroundColor: Colors.white,
+
+            ),
+            home: Container(),
+          );
+        }
+    );
+  }
+}
+
