c1ee969a7aeccbdc190d50601306c204fcafbd7e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-28 01:45:32 +0100

    Updating firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:tapcard/controllers/home_controller.dart';
+import 'package:tapcard/firebase_options.dart';
 import 'package:tapcard/utils/themes.dart';
-import 'package:tapcard/views/settings/settings_view.dart';
+
 import 'package:tapcard/views/splash_screen.dart';
 
-void main() {
-  Get.put(HomeController());
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform, );
+  
   runApp(const TapCardApp());
 }
 class TapCardApp extends StatelessWidget {
